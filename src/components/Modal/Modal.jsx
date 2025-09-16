594e95a30d3bf8074cd85227d8d2bd405bcf8b8b
Valyushka2023 <vvpto@ukr.net>
2025-09-16 22:55:39 +0300

    Travel-trucks-pet01
    ch16092255

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,3 +1,59 @@
+// import { useEffect } from 'react';
+// import PropTypes from 'prop-types';
+
+// import Logo from '../Ui/Logo/Logo.jsx';
+// import CloseButton from '../Ui/Buttons/CloseButton/CloseButton.jsx';
+// import css from './Modal.module.css';
+
+// function Modal({ title, titleClassName, children, onClose }) {
+//   useEffect(() => {
+//     // Блокуємо прокрутку body
+//     const originalOverflow = document.body.style.overflow;
+//     document.body.style.overflow = 'hidden';
+
+//     const handleEscape = e => {
+//       if (e.key === 'Escape') onClose();
+//     };
+//     window.addEventListener('keydown', handleEscape);
+
+//     return () => {
+//       // Повертаємо прокрутку після закриття
+//       document.body.style.overflow = originalOverflow;
+//       window.removeEventListener('keydown', handleEscape);
+//     };
+//   }, [onClose]);
+
+//   const handleModalContentClick = e => {
+//     e.stopPropagation();
+//   };
+
+//   return (
+//     <div className={css.backdrop} onClick={onClose}>
+//       <div className={css.modal} onClick={handleModalContentClick}>
+//         <div className={css.modalHeaderContainer}>
+//           <Logo className={css.logoInModal} />
+//           <CloseButton
+//             onClick={onClose}
+//             className={css.closeBtn}
+//             ariaLabel="Закрити модальне вікно"
+//           />
+//         </div>
+//         <h2 className={`${css.modalTitle} ${titleClassName || ''}`}>{title}</h2>
+//         <div className={css.modalBody}>{children}</div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// Modal.propTypes = {
+//   title: PropTypes.string.isRequired,
+//   titleClassName: PropTypes.string,
+//   children: PropTypes.node.isRequired,
+//   onClose: PropTypes.func.isRequired,
+// };
+
+// export default Modal;
+/**************************************** */
 import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
