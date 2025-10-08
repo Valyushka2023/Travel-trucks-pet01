080b58ca9beff5220862607a0d3ff0297f47ad29
Valyushka2023 <vvpto@ukr.net>
2025-10-08 23:21:33 +0300

    Travel-trucks-pet01
    ch08102321

--- src/components/Contacts/PhoneNumber/PhoneNumber.jsx
+++ src/components/Contacts/PhoneNumber/PhoneNumber.jsx
@@ -4,7 +4,7 @@ import css from './PhoneNumber.module.css';
 function PhoneNumber() {
   return (
     <div className={css.phone}>
-      <FaPhoneAlt className={css['phone-icon']} />
+      <FaPhoneAlt className={css['home-phone-icon']} />
       <a href="tel:0671234567" className={css['phone-number']}>
         067-123-45-67
       </a>
