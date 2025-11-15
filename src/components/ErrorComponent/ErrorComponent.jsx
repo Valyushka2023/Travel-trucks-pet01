76aeaf98bafc8c35115ac6ee236cf881772592b2
Valyushka2023 <vvpto@ukr.net>
2025-11-15 22:12:37 +0200

    Travel-trucks-pet01
    ch15112212

--- src/components/ErrorComponent/ErrorComponent.jsx
+++ src/components/ErrorComponent/ErrorComponent.jsx
@@ -1,8 +1,34 @@
+// import { useTranslation } from 'react-i18next';
+// import PropTypes from 'prop-types';
+// import css from './ErrorComponent.module.css';
+
+// const ErrorComponent = ({ error, onRetry }) => {
+//   const { t } = useTranslation('error_component');
+
+//   return (
+//     <div className={css['error-container']}>
+//       <p className={css['error-message']}>
+//         {t('error_message')} {error}
+//       </p>
+//       <button className={css['retry-button']} onClick={onRetry}>
+//         {t('retry_button')}
+//       </button>
+//     </div>
+//   );
+// };
+
+// ErrorComponent.propTypes = {
+//   error: PropTypes.string.isRequired,
+//   onRetry: PropTypes.func.isRequired,
+// };
+
+// export default ErrorComponent;
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import css from './ErrorComponent.module.css';
 
 const ErrorComponent = ({ error, onRetry }) => {
+  // >>> ВИПРАВЛЕННЯ: Змінено 'errorComponent' на 'error_component'
   const { t } = useTranslation('error_component');
 
   return (
@@ -12,6 +38,7 @@ const ErrorComponent = ({ error, onRetry }) => {
       </p>
       <button className={css['retry-button']} onClick={onRetry}>
         {t('retry_button')}
+        {/* Можна прибрати { ns: 'errorComponent' }, оскільки неймспейс вже визначений у useTranslation */}
       </button>
     </div>
   );
